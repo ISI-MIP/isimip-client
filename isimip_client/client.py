e4e4d077d729adaa0192dcff65c6bd2d7688123f
Jochen Klar <mail@jochenklar.de>
2024-12-10 13:07:41 +0100

    Include Files API v2 and refactor to use mixins, add logging

--- isimip_client/client.py
+++ isimip_client/client.py
@@ -1,10 +1,14 @@
 import hashlib
+import json
+import logging
 import time
 import zipfile
 from pathlib import Path
 from urllib.parse import urlparse
 
 import requests
+
+logger = logging.getLogger(__name__)
 
 
 class HTTPClient:
@@ -17,7 +21,7 @@ class HTTPClient:
             response.raise_for_status()
             return response.json()
         except requests.exceptions.HTTPError as e:
-            print(response.content)
+            logger.error(response.content)
             raise e
 
     def get(self, url, params={}):
@@ -49,11 +53,11 @@ class RESTClient(HTTPClient):
         if 'list_route' in kwargs:
             url += kwargs.pop('list_route').rstrip('/') + '/'
         elif 'nested_route' in kwargs:
-            url += '%s/' % kwargs.pop('parent_pk')
+            url += '{}/'.format(kwargs.pop('parent_pk'))
             url += kwargs.pop('nested_route').rstrip('/') + '/'
 
         if pk:
-            url += '%s/' % pk
+            url += f'{pk}/'
 
         if 'detail_route' in kwargs:
             url += kwargs.pop('detail_route').rstrip('/') + '/'
@@ -81,16 +85,7 @@ class RESTClient(HTTPClient):
         return self.delete(url, pk)
 
 
-class ISIMIPClient(RESTClient):
-
-    def __init__(self, data_url='https://data.isimip.org/api/v1', files_api_url='https://files.isimip.org/api/v1',
-                 auth=None, headers={}):
-        self.data_url = data_url
-        self.files_api_url = files_api_url
-
-        self.base_url = data_url
-        self.auth = auth
-        self.headers = {}
+class DataApiMixin:
 
     def datasets(self, **kwargs):
         return self.list('/datasets', **kwargs)
@@ -104,7 +99,250 @@ class ISIMIPClient(RESTClient):
     def file(self, pk, **kwargs):
         return self.retrieve('/files', pk, **kwargs)
 
-    def download(self, url, path=None, validate=True, extract=True):
+
+class FilesApiMixin:
+
+    def check(self, version):
+        if self.files_api_version != version:
+            raise RuntimeError(f'This method is only available in {version} of the Files API. '
+                               f'Please set "files_api_version=\'{version}\'".')
+
+    def post_job(self, data, uploads=None, poll=None):
+        logger.info(f'job submitted data={data} uploads={uploads}')
+
+        if uploads is None:
+            response = requests.post(self.files_api_url, json=data, auth=self.auth, headers=self.headers)
+        else:
+            files = {'data': json.dumps(data)}
+            for upload in uploads:
+                upload_path = Path(upload).expanduser()
+                files[upload_path.name] = upload_path.read_bytes()
+            response = requests.post(self.files_api_url, files=files, auth=self.auth, headers=self.headers)
+
+
+        job = self.parse_response(response)
+        self.log_job(job)
+
+        if poll and job['status'] in ['queued', 'started']:
+            time.sleep(poll)
+            return self.get_job(job['job_url'], poll=poll)
+        else:
+            return job
+
+    def get_job(self, job_url, poll=None):
+        response = requests.get(job_url, auth=self.auth, headers=self.headers)
+
+        job = self.parse_response(response)
+        self.log_job(job)
+
+        if poll and job['status'] in ['queued', 'started']:
+            time.sleep(poll)
+            return self.get_job(job['job_url'], poll=poll)
+        else:
+            return job
+
+    def log_job(self, job):
+        if job['status'] == 'finished':
+            logger.info('job {id} {status} meta={meta} file_url={file_url}'.format(**job))
+        else:
+            logger.info('job {id} {status} meta={meta}'.format(**job))
+
+class FilesApiV1Mixin:
+
+    def mask(self, paths, country=None, bbox=None, landonly=None, poll=None):
+        self.check('v1')
+
+        payload = {}
+
+        if isinstance(paths, list):
+            payload['paths'] = paths
+        else:
+            payload['paths'] = [paths]
+
+        if country is not None:
+            payload['task'] = 'mask_country'
+            payload['country'] = country
+        elif bbox is not None:
+            payload['task'] = 'mask_bbox'
+            payload['bbox'] = bbox
+        elif landonly is not None:
+            payload['task'] = 'mask_landonly'
+
+        return self.post_job(payload, poll=poll)
+
+    def cutout(self, paths, bbox, poll=None):
+        self.check('v1')
+
+        payload = {
+            'task': 'cutout_bbox',
+            'bbox': bbox
+        }
+
+        if isinstance(paths, list):
+            payload['paths'] = paths
+        else:
+            payload['paths'] = [paths]
+
+        return self.post_job(payload, poll=poll)
+
+    def select(self, paths, country=None, bbox=None, point=None, poll=None):
+        self.check('v1')
+
+        payload = {}
+
+        if isinstance(paths, list):
+            payload['paths'] = paths
+        else:
+            payload['paths'] = [paths]
+
+        if country is not None:
+            payload['task'] = 'select_country'
+            payload['country'] = country
+        elif bbox is not None:
+            payload['task'] = 'select_bbox'
+            payload['bbox'] = bbox
+        elif point is not None:
+            payload['task'] = 'select_point'
+            payload['point'] = point
+
+        return self.post_job(payload, poll=poll)
+
+
+class FilesApiV2Mixin:
+
+    def submit_job(self, paths, operations, uploads, poll=None):
+        self.check('v2')
+        return self.post_job({
+            'paths': paths,
+            'operations': operations
+        }, uploads=uploads, poll=poll)
+
+    def select_bbox(self, paths, bbox, poll=None):
+        self.check('v2')
+        return self.post_job({
+            'paths': paths,
+            'operations': [
+                {
+                    'operation': 'select_bbox',
+                    'bbox': bbox
+                }
+            ]
+        }, poll=poll)
+
+    def select_point(self, paths, point, poll=None):
+        self.check('v2')
+        return self.post_job({
+            'paths': paths,
+            'operations': [
+                {
+                    'operation': 'select_point',
+                    'point': point
+                }
+            ]
+        }, poll=poll)
+
+    def mask_bbox(self, paths, bbox, poll=None):
+        self.check('v2')
+        return self.post_job({
+            'paths': paths,
+            'operations': [
+                {
+                    'operation': 'mask_bbox',
+                    'bbox': bbox
+                }
+            ]
+        }, poll=poll)
+
+    def mask_country(self, paths, country, poll=None):
+        self.check('v2')
+        return self.post_job({
+            'paths': paths,
+            'operations': [
+                {
+                    'operation': 'mask_country',
+                    'country': country
+                }
+            ]
+        }, poll=poll)
+
+    def mask_landonly(self, paths, poll=None):
+        self.check('v2')
+        return self.post_job({
+            'paths': paths,
+            'operations': [
+                {
+                    'operation': 'mask_landonly'
+                }
+            ]
+        }, poll=poll)
+
+    def mask_mask(self, paths, mask, var, compute_mean=False, output_csv=False, poll=None):
+        self.check('v2')
+        mask = Path(mask)
+        return self.post_job({
+            'paths': paths,
+            'operations': [
+                {
+                    'operation': 'mask_mask',
+                    'mask': mask.name,
+                    'compute_mean': compute_mean,
+                    'output_csv': output_csv,
+                    'var': var
+                }
+            ]
+        }, uploads=[mask], poll=poll)
+
+    def mask_shape(self, paths, shape, layer, compute_mean=False, output_csv=False, poll=None):
+        self.check('v2')
+        shape = Path(shape)
+        mask = shape.with_suffix('.nc')
+        var = f'm_{layer}'
+        return self.post_job({
+            'paths': paths,
+            'operations': [
+                {
+                    'operation': 'create_mask',
+                    'shape': shape.name,
+                    'mask': mask.name,
+                },
+                {
+                    'operation': 'mask_mask',
+                    'mask': mask.name,
+                    'compute_mean': compute_mean,
+                    'output_csv': output_csv,
+                    'var': var
+                }
+            ]
+        }, uploads=[shape], poll=poll)
+
+    def cutout_bbox(self, paths, bbox, poll=None):
+        self.check('v2')
+        return self.post_job({
+            'paths': paths,
+            'operations': [
+                {
+                    'operation': 'cutout_bbox',
+                    'bbox': bbox
+                }
+            ]
+        }, poll=poll)
+
+    def cutout_point(self, paths, point, poll=None):
+        self.check('v2')
+        return self.post_job({
+            'paths': paths,
+            'operations': [
+                {
+                    'operation': 'cutout_point',
+                    'point': point
+                }
+            ]
+        }, poll=poll)
+
+
+class DownloadMixin:
+
+    def download(self, url, path=None, validate=False, extract=True):
         headers = self.headers.copy()
 
         file_name = Path(urlparse(url).path.split('/')[-1])
@@ -147,67 +385,15 @@ class ISIMIPClient(RESTClient):
             with zipfile.ZipFile(file_path, 'r') as zip_ref:
                 zip_ref.extractall(path)
 
-    def mask(self, paths, country=None, bbox=None, landonly=None, poll=None):
-        payload = {}
 
-        if isinstance(paths, list):
-            payload['paths'] = paths
-        else:
-            payload['paths'] = [paths]
+class ISIMIPClient(DataApiMixin, FilesApiMixin, FilesApiV1Mixin, FilesApiV2Mixin, DownloadMixin, RESTClient):
 
-        if country is not None:
-            payload['task'] = 'mask_country'
-            payload['country'] = country
-        elif bbox is not None:
-            payload['task'] = 'mask_bbox'
-            payload['bbox'] = bbox
-        elif landonly is not None:
-            payload['task'] = 'mask_landonly'
+    def __init__(self, data_url='https://data.isimip.org/api/v1', files_api_url='https://files.isimip.org/api/v1',
+                 files_api_version='v1', auth=None, headers={}):
+        self.data_url = data_url
+        self.files_api_url = files_api_url
+        self.files_api_version = files_api_version
 
-        return self.post_job(payload, poll)
-
-    def cutout(self, paths, bbox, poll=None):
-        payload = {
-            'task': 'cutout_bbox',
-            'bbox': bbox
-        }
-
-        if isinstance(paths, list):
-            payload['paths'] = paths
-        else:
-            payload['paths'] = [paths]
-
-        return self.post_job(payload, poll)
-
-    def select(self, paths, country=None, bbox=None, point=None, poll=None):
-        payload = {}
-
-        if isinstance(paths, list):
-            payload['paths'] = paths
-        else:
-            payload['paths'] = [paths]
-
-        if country is not None:
-            payload['task'] = 'select_country'
-            payload['country'] = country
-        elif bbox is not None:
-            payload['task'] = 'select_bbox'
-            payload['bbox'] = bbox
-        elif point is not None:
-            payload['task'] = 'select_point'
-            payload['point'] = point
-
-        return self.post_job(payload, poll)
-
-    def post_job(self, payload, poll=None):
-        while True:
-            response = requests.post(self.files_api_url, json=payload, auth=self.auth, headers=self.headers)
-            job = self.parse_response(response)
-            if poll:
-                print('job', job['id'], job['status'], job['meta'] if job['meta'] else '')
-                if job['status'] in ['queued', 'started']:
-                    time.sleep(poll)
-                else:
-                    return job
-            else:
-                return job
+        self.base_url = data_url
+        self.auth = auth
+        self.headers = {}
